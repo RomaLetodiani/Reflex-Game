1e1ad6b5a1aa03718654f2572226ed76d3727790
RomaLetodiani <roman.letodiani03@gmail.com>
2024-03-05 19:00:06 +0400

    Update useLocalStorage.ts

--- Frontend/src/hooks/useLocalStorage.ts
+++ Frontend/src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react'
 
 /**
  * A custom React hook for managing state stored in the local storage or session storage of the browser.
@@ -12,34 +12,31 @@ import { useCallback, useEffect, useState } from 'react';
 const useStorage = <T>(
   key: string,
   initialValue: T | (() => T),
-  storageType: 'localStorage' | 'sessionStorage'
+  storageType: 'localStorage' | 'sessionStorage',
 ): [T, React.Dispatch<React.SetStateAction<T>>, () => void] => {
-  const storageObject =
-    storageType === 'localStorage'
-      ? window.localStorage
-      : window.sessionStorage;
+  const storageObject = storageType === 'localStorage' ? window.localStorage : window.sessionStorage
   const [value, setValue] = useState(() => {
-    const jsonValue = storageObject.getItem(key);
+    const jsonValue = storageObject.getItem(key)
     if (jsonValue !== null) {
-      return JSON.parse(jsonValue);
+      return JSON.parse(jsonValue)
     }
 
     if (typeof initialValue === 'function') {
-      return (initialValue as () => T)();
+      return (initialValue as () => T)()
     }
-    return initialValue;
-  });
+    return initialValue
+  })
 
   useEffect(() => {
-    if (value === undefined) return storageObject.removeItem(key);
+    if (value === undefined) return storageObject.removeItem(key)
 
-    storageObject.setItem(key, JSON.stringify(value));
-  }, [key, value, storageObject]);
+    storageObject.setItem(key, JSON.stringify(value))
+  }, [key, value, storageObject])
 
-  const remove = useCallback(() => setValue(undefined), []);
+  const remove = useCallback(() => setValue(undefined), [])
 
-  return [value, setValue, remove];
-};
+  return [value, setValue, remove]
+}
 
 /**
  * A custom React hook for managing state stored in the local storage of the browser.
@@ -51,22 +48,7 @@ const useStorage = <T>(
  */
 export const useLocalStorage = <T>(
   key: string,
-  initialValue: T | (() => T)
+  initialValue: T | (() => T),
 ): [T, React.Dispatch<React.SetStateAction<T>>, () => void] => {
-  return useStorage(key, initialValue, 'localStorage');
-};
-
-/**
- * A custom React hook for managing state stored in the session storage of the browser.
- * @template T - The type of the state value.
- * @param {string} key - The key under which the value will be stored in the session storage.
- * @param {T | (() => T)} initialValue - The initial value of the state, or a function that returns the initial value.
- * @returns {[T, React.Dispatch<React.SetStateAction<T>>, () => void]}
- * A tuple containing the current value, a function to set the value, and a function to remove the value from session storage.
- */
-export const useSessionStorage = <T>(
-  key: string,
-  initialValue: T | (() => T)
-): [T, React.Dispatch<React.SetStateAction<T>>, () => void] => {
-  return useStorage(key, initialValue, 'sessionStorage');
-};
+  return useStorage(key, initialValue, 'localStorage')
+}
